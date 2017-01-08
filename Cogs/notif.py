7dcb01a6f32336b3602e2bc9b8f62af2c1fdfb31
BlackSynder <frogermanz@gmail.com>
2017-01-08 16:20:12 +0200

    ok now thats the final one

--- Cogs/notif.py
+++ Cogs/notif.py
@@ -6,11 +6,9 @@ class NameNotifier:
         self.bot = bot
 
     async def on_message(self, message):
-        print("msg")
         if message.author.id == "236176083861372928":
             return
         if message.channel.id == "258330960733405184":
-            print("yes")
             msg = await self.bot.send_message(discord.Object(id="258330960733405184"), "<@111158853839654912>")
             await self.bot.delete_message(msg)
 
