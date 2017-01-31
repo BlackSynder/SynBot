76130a7ba61fc4d663dbf80b89215e8ae1644e27
BlackSynder <frogermanz@gmail.com>
2017-01-31 22:28:15 +0200

    fix message

--- Cogs/notif.py
+++ Cogs/notif.py
@@ -9,7 +9,7 @@ class NameNotifier:
         if message.author.id == "236176083861372928":
             return
         if message.channel.id == "258330960733405184":
-            msg = await self.bot.send_message(discord.Object(id="258330960733405184"), "<@111158853839654912>\n%s" % message)
+            msg = await self.bot.send_message(discord.Object(id="258330960733405184"), "<@111158853839654912>\n%s" % message.content)
             await self.bot.delete_message(msg)
 
 
