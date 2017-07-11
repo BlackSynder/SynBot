a26a39bfced489ec2013f095feffe9238b388e16
Synder <frogermanz@gmail.com>
2017-07-11 18:47:34 +0300

    fix notif

--- Cogs/notif.py
+++ Cogs/notif.py
@@ -5,9 +5,9 @@ class NameNotifier:
         self.bot = bot
 
     async def on_message(self, message):
-        if message.author.id == "236176083861372928":
+        if message.author.id == 236176083861372928:
             return
-        if message.channel.id == "258330960733405184":
+        if message.channel.id == 258330960733405184:
             await message.delete()
             await message.channel.send(embed=message.embeds[0])
 def setup(bot):
