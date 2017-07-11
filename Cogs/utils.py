19020bfcfbef9ce3a55fdfb2a7b4d8008af795e1
Synder <frogermanz@gmail.com>
2017-07-11 15:27:31 +0300

    added eval and exec commands

--- Cogs/utils.py
+++ Cogs/utils.py
@@ -2,6 +2,9 @@ from discord.ext import commands
 import discord
 import os
 import asyncio
+import inspect
+import textwrap
+import tokage
 
 
 class Utilities:
@@ -18,6 +21,67 @@ class Utilities:
         except Exception:
             await ctx.send(":exclamation: File not found!")
 
+    @commands.command(hidden=True, aliases=["eval", "evaluate"])
+    @commands.is_owner()
+    async def debug(self, ctx, *, code: str):
+        """Evaluates code."""
+        code = code.strip('`')
+        python = '```py\n{}\n```'
+        result = None
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'message': ctx.message,
+            'server': ctx.guild,
+            'guild': ctx.guild,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'history': await ctx.channel.history().flatten(),
+            't_client': tokage.Client()
+        }
+
+        env.update(globals())
+
+        try:
+            result = eval(code, env)
+            if inspect.isawaitable(result):
+                result = await result
+        except Exception as e:
+            await ctx.send(python.format(type(e).__name__ + ': ' + str(e)))
+            return
+
+        await ctx.send(python.format(result))
+
+    @commands.command(name="exec", hidden=True)
+    @commands.is_owner()
+    async def execute(self, ctx, *, code):
+        if code.startswith("```") and code.endswith("```"):
+            code = code.strip("```")
+            if code.startswith("py\n"):
+                code = code[3:]
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'message': ctx.message,
+            'server': ctx.guild,
+            'guild': ctx.guild,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'history': await ctx.channel.history().flatten(),
+            't_client': tokage.Client()
+        }
+        env.update(globals())
+        wrapped = 'async def func():\n%s' % textwrap.indent(code, '  ')
+        try:
+            result = exec(wrapped, env)
+            func = env['func']
+            await func()
+            if result:
+                await ctx.send(f"```{result}```")
+        except Exception as e:
+            await ctx.send(f"```{type(e).__name__ + ': ' + str(e)}```")
+    
     @commands.command()
     async def ping(self, ctx, message="Pong!"):
         await ctx.send(ctx.author.mention + " " + message)
