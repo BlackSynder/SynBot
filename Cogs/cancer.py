1c6e2d2026b3d5fa128954f31e93dc92b2309207
BlackSynder <frogermanz@gmail.com>
2017-12-21 22:38:02 +0200

    more cancer

--- Cogs/cancer.py
+++ Cogs/cancer.py
@@ -8,6 +8,11 @@ class Cancer:
             return
         await member.guild.system_channel.send("yes " + member.mention)
 
+    async def on_guild_emoji_update(self, guild, before, after):
+        if guild.id not in self.ok_list:
+            return
+        await guild.system_channel.send("the emojis were updated")
+
 
 def setup(bot):
     bot.add_cog(Cancer(bot))
