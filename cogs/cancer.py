d35aed562b3c9eba6f7de7ac4aa7d6ad7723ec0a
SynderBlack <frogermanz@gmail.com>
2019-03-31 14:00:57 +0300

    Add listnener decos

--- cogs/cancer.py
+++ cogs/cancer.py
@@ -1,21 +1,24 @@
-from discord.ext import commands
+from discord.ext.commands import Cog
 
 
-class Cancer(commands.Cog):
+class Cancer(Cog):
     def __init__(self, bot):
         self.bot = bot
         self.ok_list = [198101180180594688, 246291440106340352]
 
+    @Cog.listener
     async def on_member_join(self, member):
         if member.guild.id not in self.ok_list:
             return
         await member.guild.system_channel.send("yes " + member.mention)
 
+    @Cog.listener
     async def on_member_remove(self, member):
         if member.guild.id not in self.ok_list:
             return
         await member.guild.system_channel.send("no " + member.mention)
 
+    @Cog.listener
     async def on_guild_emojis_update(self, guild, before, after):
         if guild.id not in self.ok_list:
             return
