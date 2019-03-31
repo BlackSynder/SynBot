3b14d78cd1d6c6580cdf8acdd1b10d5b7734d82f
SynderBlack <frogermanz@gmail.com>
2019-03-31 15:53:54 +0300

    fun

--- cogs/cancer.py
+++ cogs/cancer.py
@@ -6,19 +6,19 @@ class Cancer(Cog):
         self.bot = bot
         self.ok_list = [198101180180594688, 246291440106340352]
 
-    @Cog.listener
+    @Cog.listener()
     async def on_member_join(self, member):
         if member.guild.id not in self.ok_list:
             return
         await member.guild.system_channel.send("yes " + member.mention)
 
-    @Cog.listener
+    @Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id not in self.ok_list:
             return
         await member.guild.system_channel.send("no " + member.mention)
 
-    @Cog.listener
+    @Cog.listener()
     async def on_guild_emojis_update(self, guild, before, after):
         if guild.id not in self.ok_list:
             return
